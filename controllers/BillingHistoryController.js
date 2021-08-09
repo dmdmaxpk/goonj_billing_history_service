aa859ebc3e0b4355e22ea3cf5e9910518d06e612
Taha <taha@dmdmax.com.pk>
2021-08-09 15:23:50 +0500

    log removed

--- controllers/BillingHistoryController.js
+++ controllers/BillingHistoryController.js
@@ -2,7 +2,6 @@ const BillingHistoryRepository = require('../repos/BillingHistoryRepository');
 const historyRepo = new BillingHistoryRepository();
 
 exports.getExpiryHistory = async(req, res) => {
-    console.log("req", req.query)
     let result = await historyRepo.getExpiryHistory(req.query.user_id)
     res.send(result);
 }
