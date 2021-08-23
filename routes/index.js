82546d0cdbf85208fe241630d0fc622effdf183e
taha <taha@dmdmax.com>
2021-08-23 00:52:54 -0700

    report routes added

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get('/', (req, res) => res.send("Goonj Billing History Service"));
 
 router.use('/history',    require('./historyRoutes'));
+router.use('/reports',    require('./reportRoutes'));
 
 module.exports = router;
