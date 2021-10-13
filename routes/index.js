b9c0a863ad6c275b6a049763f2ed90b6c1e12718
taha <taha@dmdmax.com>
2021-10-13 17:40:38 +0500

    billing stats APIs added

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/BillingHistoryController');
-
+const cors = require('cors');
 // Service Label
 router.get('/', (req, res) => res.send("Goonj Billing History Microservice"));
 
 router.get('/history/count', controller.getHistoryCount);
+
+router.route('/rev', cors())
+    .get(controller.rev_report)
+
+router.route('/req-count', cors())
+    .get(controller.req_count)
+
+router.route('/revenue/stats', cors())
+    .get(controller.revenue_stats);
 
 module.exports = router;
